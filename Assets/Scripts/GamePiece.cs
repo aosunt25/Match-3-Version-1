c1fb4f741f1e8c9f13d4fdf5711223b46d9e66a9
aosunat251 <alfredoosuna.40@gmail.com>
2019-02-25 22:20:12 -0600

    commit inicial

---
+++ Assets/Scripts/GamePiece.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePiece : MonoBehaviour
+{
+
+    private int x;
+    private int y;
+
+    public int X
+    {
+        get { return x; }
+        set
+        {
+            if (IsMovable())
+            {
+                x = value;
+            }
+        }
+    }
+
+    public int Y
+    {
+        get { return y; }
+        set
+        {
+            if (IsMovable())
+            {
+                y = value;
+            }
+        }
+    }
+
+
+    public bool IsMovable()
+    {
+        return movableComponent != null;
+    }
+
+    public bool IsClearable()
+    {
+        return clearableComponent != null;
+    }
+
+
+    private Grid.PieceType type;
+
+    public Grid.PieceType Type
+    {
+        get { return type; }
+    }
+
+    private Grid grid;
+
+    public Grid GridRef
+    {
+        get { return grid; }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Init(int _x, int _y, Grid _grid, Grid.PieceType _type)
+    {
+        x = _x;
+        y = _y;
+        grid = _grid;
+        type = _type;
+    }
+
+
+    private MovePiece movableComponent;
+
+    public MovePiece MovableComponent
+    {
+        get { return movableComponent; }
+    }
+
+
+    private ColorPeice colorComponent;
+
+    public ColorPeice ColorComponent
+    {
+        get { return colorComponent; }
+    }
+
+    public bool IsColored()
+    {
+        return colorComponent != null;
+    }
+
+    private ClearablePiece clearableComponent;
+
+    public ClearablePiece ClearableComponent
+    {
+        get { return clearableComponent; }
+    }
+
+    private void Awake()
+    {
+        movableComponent = GetComponent<MovePiece>();
+        colorComponent = GetComponent<ColorPeice>();
+        clearableComponent = GetComponent<ClearablePiece>();
+
+    }
+
+    private void OnMouseEnter()
+    {
+        grid.EnteredPiece(this);
+    }
+    private void OnMouseDown()
+    {
+        grid.PressPiece(this);
+    }
+    private void OnMouseUp()
+    {
+        grid.ReleasePiece();
+    }
+}
---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,404 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic; //Access to the dictionary class
+
+
+public class Grid : MonoBehaviour {
+
+   
+   //This is use to know what kind of pieces we can have in it
+   public enum PieceType
+    {
+        EMPTY,
+        NORMAL,
+        COUNT, //How many kind of piece type we have
+    };
+
+    public int xDim;
+    public int yDim;
+    public float fillTime;
+
+    private GamePiece pressedPiece;
+    private GamePiece enteredPiece;
+
+    private Dictionary<PieceType, GameObject> piecePrefabDict;
+
+    public PiecePrefab[] piecePrefabs;
+    public GameObject backgroundPrefab; //background for every piece
+
+    private GamePiece[,] pieces;
+
+    [System.Serializable] //Our custom struct will show in the inspecto
+    public struct PiecePrefab
+    {
+        public PieceType type;
+        public GameObject prefab;
+    };
+
+
+    // Use this for initialization
+    void Start () {
+
+        piecePrefabDict = new Dictionary<PieceType, GameObject>();
+        for (int i =0; i <piecePrefabs.Length; i++)
+        {
+            if(!piecePrefabDict.ContainsKey(piecePrefabs[i].type))
+            {
+                piecePrefabDict.Add(piecePrefabs[i].type, piecePrefabs[i].prefab); //If the dictionary does not contain the key, we add it in the dictionary
+            }
+        }
+
+        for (int x = 0; x < xDim; x++)
+        {
+            for (int y = 0; y < yDim; y++)
+            {
+                GameObject background = (GameObject)Instantiate(backgroundPrefab,GetPositionGird(x,y), Quaternion.identity);
+                background.transform.parent = transform;
+             }
+        }
+
+        pieces = new GamePiece[xDim, yDim];
+
+        for (int x = 0; x < xDim; x++)
+        {
+            for (int y = 0; y < yDim; y++)
+            {
+                /*GameObject newPiece = (GameObject)Instantiate(piecePrefabDict[PieceType.NORMAL], GetPositionGird(x, y), Quaternion.identity);
+                 newPiece.name = "Piece(" + x + "," + y + ")";
+                 newPiece.transform.parent = transform;
+
+                 pieces[x, y] = newPiece.GetComponent<GamePiece>();
+                 pieces[x, y].Init(x, y, this, PieceType.NORMAL);
+
+                 if (pieces[x, y].IsMovable())
+                 {
+                     pieces[x, y].MovableComponent.Move(x, y);
+                 }
+                 if (pieces[x, y].IsColored())
+                 {
+                     pieces[x, y].ColorComponent.SetColor((ColorPeice.ColorType)Random.Range(0,pieces[x,y].ColorComponent.NumColors));
+                 }*/
+                SpawnNewPiece(x,y,PieceType.EMPTY);
+
+            }
+        }
+
+        StartCoroutine(Fill());
+    }
+	
+
+    public Vector2 GetPositionGird(int x, int y)
+    {
+        return new Vector2(transform.position.x - xDim / 2.0f + x,
+            transform.position.y + yDim / 2.0f - y);
+    }
+
+
+    public GamePiece SpawnNewPiece(int x, int y, PieceType type)
+    {
+        GameObject newPiece = (GameObject)Instantiate(piecePrefabDict[type], GetPositionGird(x, y), Quaternion.identity);
+        newPiece.transform.parent = transform;
+        pieces[x, y] = newPiece.GetComponent<GamePiece>();
+        pieces[x, y].Init(x, y, this, type);
+
+        return pieces[x, y];
+    }
+
+
+    public IEnumerator Fill()
+    {
+
+
+        bool needsRefill = true;
+
+        while (needsRefill)
+        {
+            yield return new WaitForSeconds(fillTime);
+
+            while (FillStep())
+            {
+
+                yield return new WaitForSeconds(fillTime);
+            }
+
+            needsRefill = ClearAllValidMatches();
+        }
+
+    }
+
+    public bool FillStep()
+    {
+        bool movedPiece = false;
+
+        for (int y = yDim - 2; y >= 0; y--)
+        {
+            for (int x = 0; x < xDim; x++)
+            {
+                GamePiece piece = pieces[x, y];
+
+                if (piece.IsMovable())
+                {
+                    GamePiece pieceBelow = pieces[x, y + 1];
+
+                    if (pieceBelow.Type == PieceType.EMPTY)
+                    {
+                        Destroy(pieceBelow.gameObject);
+                        piece.MovableComponent.Move(x, y + 1, fillTime);
+                        pieces[x, y + 1] = piece;
+                        SpawnNewPiece(x, y, PieceType.EMPTY);
+                        movedPiece = true;
+                    }
+                }
+            }
+        }
+        for (int x = 0; x < xDim; x++)
+        {
+            GamePiece pieceBelow = pieces[x, 0];
+
+            if (pieceBelow.Type == PieceType.EMPTY)
+            {
+                Destroy(pieceBelow.gameObject);
+                GameObject newPiece = (GameObject)Instantiate(piecePrefabDict[PieceType.NORMAL], GetPositionGird(x, -1), Quaternion.identity);
+                newPiece.transform.parent = transform;
+
+                pieces[x, 0] = newPiece.GetComponent<GamePiece>();
+                pieces[x, 0].Init(x, -1, this, PieceType.NORMAL);
+                pieces[x, 0].MovableComponent.Move(x, 0, fillTime);
+                pieces[x, 0].ColorComponent.SetColor((ColorPeice.ColorType)Random.Range(0, pieces[x, 0].ColorComponent.NumColors));
+                movedPiece = true;
+            }
+        }
+
+        return movedPiece;
+    }
+
+
+    public bool isAdjacent(GamePiece piece1, GamePiece piece2)
+    {
+        return (piece1.X == piece2.X && (int)Mathf.Abs(piece1.Y - piece2.Y) == 1) || (piece1.Y == piece2.Y && (int)Mathf.Abs(piece1.X - piece2.X) == 1);
+    }
+
+    public void SwapPieces(GamePiece piece1, GamePiece piece2)
+    {
+        if (piece1.IsMovable() && piece2.IsMovable())
+        {
+            pieces[piece1.X, piece1.Y] = piece2;
+            pieces[piece2.X, piece2.Y] = piece1;
+
+           
+
+            if (GetMatch(piece1, piece2.X, piece2.Y) != null || GetMatch(piece2, piece1.X, piece1.Y) != null)
+            {
+
+                int piece1X = piece1.X;
+                int piece1Y = piece1.Y;
+
+                piece1.MovableComponent.Move(piece2.X, piece2.Y, fillTime);
+                piece2.MovableComponent.Move(piece1X, piece1Y, fillTime);
+
+                ClearAllValidMatches();
+
+                StartCoroutine(Fill());
+
+            }
+            else
+            {
+
+                pieces[piece1.X, piece1.Y] = piece1;
+                pieces[piece2.X, piece2.Y] = piece2;
+
+                //AQUI SE SUPONe QUE VA LA SECUENCIA
+
+                /*int piece1X = piece1.X;
+                int piece1Y = piece1.Y;
+                int piece2X = piece2.X;
+                int piece2Y = piece2.Y;
+
+                piece1.MovableComponent.Move(piece2.X, piece2.Y, fillTime);
+                piece2.MovableComponent.Move(piece1X, piece1Y, fillTime);
+
+                ExecuteAfterTime(4);
+
+                piece2.MovableComponent.Move(piece, piece2.Y, fillTime);
+                piece1.MovableComponent.Move(piece1X, piece1Y, fillTime);
+
+                pieces[piece1.X, piece1.Y] = piece1;
+                pieces[piece2.X, piece2.Y] = piece2;*/
+            }
+        }
+    }
+    public void  PressPiece (GamePiece piece)
+    {
+        pressedPiece = piece;
+    }
+    public void EnteredPiece(GamePiece piece)
+    {
+        enteredPiece = piece;
+    }
+
+    public void ReleasePiece()
+    {
+        if (isAdjacent(pressedPiece, enteredPiece))
+        {
+            SwapPieces(pressedPiece, enteredPiece);
+        }
+    }
+
+    public List<GamePiece> GetMatch(GamePiece piece, int newX, int newY)
+    {
+        if (piece.IsColored())
+        {
+            ColorPeice.ColorType color = piece.ColorComponent.Color;
+            List<GamePiece> horizontalPieces = new List<GamePiece>();
+            List<GamePiece> verticalPieces = new List<GamePiece>();
+            List<GamePiece> matchingPieces = new List<GamePiece>();
+
+            // Para checar primero en el eje horizontal
+            horizontalPieces.Add(piece);
+
+            for (int dir = 0; dir <= 1; dir++)
+            {
+                for (int xOffset = 1; xOffset < xDim; xOffset++)
+                {
+                    int x;
+
+                    if (dir == 0)
+                    { // izquierda
+                        x = newX - xOffset;
+                    }
+                    else
+                    { // derecha
+                        x = newX + xOffset;
+                    }
+
+                    if (x < 0 || x >= xDim)
+                    {
+                        break;
+                    }
+
+                    if (pieces[x, newY].IsColored() && pieces[x, newY].ColorComponent.Color == color)
+                    {
+                        horizontalPieces.Add(pieces[x, newY]);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            if (horizontalPieces.Count >= 3)
+            {
+                for (int i = 0; i < horizontalPieces.Count; i++)
+                {
+                    matchingPieces.Add(horizontalPieces[i]);
+                }
+            }
+
+            if (matchingPieces.Count >= 3)
+            {
+                return matchingPieces;
+            }
+
+            // Si llega aquí, nos toca checar en el eje vertical
+            verticalPieces.Add(piece);
+
+            for (int dir = 0; dir <= 1; dir++)
+            {
+                for (int yOffset = 1; yOffset < yDim; yOffset++)
+                {
+                    int y;
+
+                    if (dir == 0)
+                    { // Arriba
+                        y = newY - yOffset;
+                    }
+                    else
+                    { // Abajo
+                        y = newY + yOffset;
+                    }
+
+                    if (y < 0 || y >= yDim)
+                    {
+                        break;
+                    }
+
+                    if (pieces[newX, y].IsColored() && pieces[newX, y].ColorComponent.Color == color)
+                    {
+                        verticalPieces.Add(pieces[newX, y]);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            if (verticalPieces.Count >= 3)
+            {
+                for (int i = 0; i < verticalPieces.Count; i++)
+                {
+                    matchingPieces.Add(verticalPieces[i]);
+                }
+            }
+
+            if (matchingPieces.Count >= 3)
+            {
+                return matchingPieces;
+            }
+        }
+
+        return null;
+    }
+
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        // Code to execute after the delay
+    }
+
+    public bool ClearAllValidMatches()
+    {
+        bool needsRefill = false;
+
+        for (int y = 0; y < yDim; y++)
+        {
+            for (int x = 0; x < xDim; x++)
+            {
+                if (pieces[x, y].IsClearable())
+                {
+                    List<GamePiece> match = GetMatch(pieces[x, y], x, y);
+
+                    if (match != null)
+                    {
+                        for (int i = 0; i < match.Count; i++)
+                        {
+                            if (ClearPiece(match[i].X, match[i].Y))
+                            {
+                                needsRefill = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return needsRefill;
+    }
+
+    public bool ClearPiece(int x, int y)
+    {
+        if (pieces[x, y].IsClearable() && !pieces[x, y].ClearableComponent.IsBeingCleared)
+        {
+            pieces[x, y].ClearableComponent.Clear();
+            SpawnNewPiece(x, y, PieceType.EMPTY);
+
+            return true;
+        }
+
+        return false;
+    }
+
+
+}
